278c8ccd4aede72d67a1ce56efdf81cc666ffece
DarthTorus <darth.torus@gmail.com>
2016-12-19 20:52:58 -0500

    Added Materials file
    + materials.json

--- minecraft.js
+++ minecraft.js
@@ -1,13 +1,21 @@
 var bot = process.DiscordBot;
-
+var mat = require("./materials.json");
+/*
+B = Brewing
+C = Crafting
+F = Farming
+S = Smelting
+M = Mining (W/O SILK TOUCH)
+N = Not obtainable w/o cheats
+*/
 function minecraftCheck(m, cI) {
-  console.log(m[0]);
+	console.log(m[0]);
 	switch (m[0]) {
-    case 'craft':
-      break;
-    default:
-    //just do nothing
-  }
+		case 'craft':
+			break;
+		default:
+			//just do nothing
+	}
 }
 
 var mcFunctions = {
