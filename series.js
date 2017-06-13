d72cdf9feff2e94e16bf0efb9834135ba1f3919b
DarthTorus <darth.torus@gmail.com>
2017-06-12 20:16:58 -0400

    Series functions
    + Lucas Number series
    + Collatz Conjecture
    + Triangular numbers

--- series.js
+++ series.js
@@ -11,7 +11,20 @@ function seriesCheck(m,cID) {
     case 'fibonacci':
       m.shift();
       getNthFib(m,cID);
-      break
+      break;
+    case 'lucas':
+      m.shift();
+      getNthLucas(m,cID);
+      break;
+    case 'tri':
+    case 'triangle':
+      m.shift();
+      getNthTriangle(m,cID);
+      break;
+    case 'collatz':
+      m.shift();
+      getCollatzChain(m,cID);
+      break;
     default:
       //Do nothing b/c of user error
   }
@@ -38,6 +51,69 @@ function getNthFib(msg, cI) {
   bot.sendMessages(cI, [msgText]);
 }
 
+// Gets the nth triangular number
+function getNthTriangle(msg, cI) {
+  var n = Number(msg[0]);
+  var result = (n*n + n)*.5;
+
+  var msgText = "`";
+  if(n % 10 == 1) {
+    msgText += (n + "st");
+  } else if(n % 10 == 2) {
+    msgText += (n + "nd");
+  } else if(n % 10 == 3) {
+    msgText += (n + "rd");
+  } else {
+    msgText += (n + "th");
+  }
+  msgText += " term is: " + result +"`";
+  bot.sendMessages(cI, [msgText]);
+}
+
+// Gets the Collatz conjecture chain starting from N
+function getCollatzChain(msg,cI) {
+  var msgText = ""
+  var n = Number(msg[0]);
+  msgText += "```"+ n + " → ";
+  while(n > 1) {
+    if(n % 2 == 1) {
+
+      n = (n*3)+1;
+      msgText += n;
+      msgText += " → ";
+    }
+    else {
+      n /= 2;
+      msgText += n;
+      if(n != 1) {
+        msgText += " → ";
+      }
+    }
+  }
+  msgText += "```";
+
+  bot.sendMessages(cI, [msgText]);
+}
+
+function getNthLucas(msg, cI) {
+  var n = Number(msg[0]);
+  var term1 = Math.pow(POS_PHI,n);
+  var term2 = Math.pow(NEG_PHI,n);
+  var result = Math.round((term1+term2));
+
+  var msgText = "`";
+  if(n % 10 == 1) {
+    msgText += (n + "st");
+  } else if(n % 10 == 2) {
+    msgText += (n + "nd");
+  } else if(n % 10 == 3) {
+    msgText += (n + "rd");
+  } else {
+    msgText += (n + "th");
+  }
+  msgText += " term is: " + result +"`";
+  bot.sendMessages(cI, [msgText]);
+}
 
 var seriesFunctions = {
 	seriesCheck: seriesCheck
