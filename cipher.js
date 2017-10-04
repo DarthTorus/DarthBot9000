0bd67c8a071c6a6fd363dfcee7ce8b7d4170f00a
ipie4fun <ipie4fun@gmail.com>
2017-10-04 18:41:34 -0500

    S P A C E C O M M A
    Added some commas I forgot in the matrix...

--- cipher.js
+++ cipher.js
@@ -1,15 +1,15 @@
 //Hello me, let's try not to derp this file creation up this time, k?
 //ipie4fun's biggest controbution so far (10/4/17)
 var bot = process.DiscordBot; //makes bot things work
-var mat = [[":","9","k","+","i","c","F","n","u"]
-	   ["I","m","B","G","8","{","&","j","y"]
-	   ["C","X","}",")","Z","s","l","$","J"]
-	   ["r","z","\"","D","U","]","2","%","K"]
-	   ["[","/","-",",","6","@","R","M","Q"]
-	   ["W","e","h","&","f","o","A","5","w"]
-	   [">","?",";","<","O","!","1","0","a"]
-	   ["(",".","#","~","N","E","q","^","7"]
-	   ["P","d","S","V","3","b","H","T","L"]
+var mat = [[":","9","k","+","i","c","F","n","u"],
+	   ["I","m","B","G","8","{","&","j","y"],
+	   ["C","X","}",")","Z","s","l","$","J"],
+	   ["r","z","\"","D","U","]","2","%","K"],
+	   ["[","/","-",",","6","@","R","M","Q"],
+	   ["W","e","h","&","f","o","A","5","w"],
+	   [">","?",";","<","O","!","1","0","a"],
+	   ["(",".","#","~","N","E","q","^","7"],
+	   ["P","d","S","V","3","b","H","T","L"],
 	   ["t","g","v","4","p","\\","Y","x","="]]; //Darth, if you somehow make this matrix private, make sure to compeltely re-randomise the matrix.
 //TODO: Enter msg.length <= 1 error messages.
 
