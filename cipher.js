4adc6ef477d64f26d5ca4b6170cb6fed05cac324
ipie4fun <ipie4fun@gmail.com>
2017-10-04 10:40:26 -0500

    Added matrix
    I added the matrix key of the encryption and decryption

--- cipher.js
+++ cipher.js
@@ -1,8 +1,18 @@
 //Hello me, let's try not to derp this file creation up this time, k?
 //ipie4fun's biggest controbution so far (10/4/17)
 var bot = process.DiscordBot; //makes bot things work
-
-//TODO: Enter msg.;ength <= 1 error messages.
+var mat = [[":","9","k","+","i","c","F","n","u"]
+	   ["I","m","B","G","8","{","&","j","y"]
+	   ["C","X","}",")","Z","s","l","$","J"]
+	   ["r","z","\"","D","U","]","2","%","K"]
+	   ["[","/","-",",","6","@","R","M","Q"]
+	   ["W","e","h","&","f","o","A","5","w"]
+	   [">","?",";","<","O","!","1","0","a"]
+	   ["(",".","#","|","N","E","q","^","7"]
+	   ["P","d","S","V","3","b","H","T","L"]
+	   ["t","g","v","4","p","\\","Y","x","="]];
+}
+//TODO: Enter msg.length <= 1 error messages.
 
 function doThing(msg, uID)
 {
@@ -21,11 +31,13 @@ function doThing(msg, uID)
     if(msg[0].equals("encrypt"))
     {
       msg.split();
+      msg.join(" ");
       encrypt(msg, uID);
     }
     else if(msg[0].equals("decrypt"))
     {
       msg.split();
+      msg.join(" ");
       encrypt(msg, uID);
     }
     else
