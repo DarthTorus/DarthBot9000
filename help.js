b3c39f01a98af6d4e66d1c569ca1b402a72be780
AstroSnail <astrosnail@protonmail.com>
2020-08-19 12:26:36 +0000

    help.js: Implement help for subcommands
    This implementation can handle arbitrarily many subcommands when needed.
    It might be sensible to redo getHelpText() so that it has this capacity
    as well.

--- help.js
+++ help.js
@@ -3,30 +3,46 @@ var formatCode = "diff\n";
 var help = require("./help.json");
 
 function checkHelp(m, message) {
-	var c = m[0];
+	let c = m[0];
 
-	if(c != '') {
-		if(help.hasOwnProperty(c)) {
-			var hDesc = "```Command: "+bot.trigger+c;
-			// Assumes every command has an alias array.
-			// Maybe add check?
-			for (alias of help[c].alias) {
-				hDesc += " | " + alias;
-			}
-			hDesc += "\n";
-			hDesc += ("Description: " + help[m[0]].desc + "\n");
-			hDesc += "```";
-			try {
-				message.author.send(hDesc);
-			}catch(err) {
-				message.channel.send("I couldn't send a help message to your DMs. Perhaps you have DMs turned off?")
-			}
-		}
-	}
-	else {
+	if (c == '') {
 		sendHelp(message);
+	} else if (help.hasOwnProperty(c)) {
+		let [cHelp, n] = recursiveHelp(help[c], m, 1);
+		if (n < m.length) {
+			// Requested help for a subcommand
+			// which does not exist in help.json.
+			// Send nothing.
+			// Possible alternatives: send help
+			// of what *does* exist, or sendHelp.
+			return
+		}
+		// This line might look a bit too fancy.
+		// It probably is.
+		let hDesc = "```Command: " + bot.trigger + m.slice(0, n).join(" ") + cHelp.alias.reduce(((a, v) => a + " | " + v), "") + "\n";
+		hDesc += "Description: " + cHelp.desc + "\n";
+		hDesc += "```";
+		try {
+			message.author.send(hDesc);
+		} catch(err) {
+			message.channel.send("I couldn't send a help message to your DMs. Perhaps you have DMs turned off?")
+		}
+	} else {
+		// Requested help for a command which does
+		// not exist in help.json.
+		// Send nothing.
+		// Possible alternative: sendHelp.
+		return
 	}
+}
 
+function recursiveHelp(base, m, n) {
+	let next = m[n];
+	if (base.commands.hasOwnProperty(next)) {
+		return recursiveHelp(base.commands[next], m, n+1);
+	} else {
+		return [base, n];
+	}
 }
 
 function sendHelp(message) {
