33f859917bc926ed5de258ea78d2401dfdc05818
DarthTorus <darth.torus@gmail.com>
2020-10-20 09:52:35 -0400

    Removed unnecessary bot methods

--- formubot.js
+++ formubot.js
@@ -33,7 +33,6 @@ const url = require('url');
 const request = require('request');
 //Bot properties declared
 requireFiles();
-bot.sendMessages = sendMessages;
 bot.colors = colors;
 bot.PNGImage = PNGImage;
 var logText = "";
@@ -217,63 +216,7 @@ bot.on("debug", function(rawEvent) {
 // });
 
 /*Function declaration area*/
-function sendMessages(ID, messageArr, interval) {
-	var callback, resArr = [],
-		len = messageArr.length;
-	typeof(arguments[2]) === 'function' ? callback = arguments[2]: callback = arguments[3];
-	if (typeof(interval) !== 'number') interval = 1000;
 
-	function _sendMessages() {
-		setTimeout(function() {
-			if (messageArr[0]) {
-				bot.sendMessage({
-					to: ID,
-					typing: false,
-					message: messageArr.shift()
-				}, function(err, res) {
-					if (err) {
-						resArr.push(err);
-					} else {
-						resArr.push(res);
-					}
-					if (resArr.length === len)
-						if (typeof(callback) === 'function') callback(resArr);
-				});
-				_sendMessages();
-			}
-		}, interval);
-	}
-	_sendMessages();
-}
-
-function sendFiles(ID, fileArr, interval) {
-	var callback, resArr = [],
-		len = fileArr.length;
-	typeof(arguments[2]) === 'function' ? callback = arguments[2]: callback = arguments[3];
-	if (typeof(interval) !== 'number') interval = 1000;
-
-	function _sendFiles() {
-		setTimeout(function() {
-			if (fileArr[0]) {
-				bot.uploadFile({
-					to: ID,
-					typing: false,
-					file: fileArr.shift()
-				}, function(err, res) {
-					if (err) {
-						resArr.push(err);
-					} else {
-						resArr.push(res);
-					}
-					if (resArr.length === len)
-						if (typeof(callback) === 'function') callback(resArr);
-				});
-				_sendFiles();
-			}
-		}, interval);
-	}
-	_sendFiles();
-}
 
 bot.disconnect= function(message) {
 	message.channel.send("Powering down for now!");
